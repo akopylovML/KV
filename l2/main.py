f5a7d1d7c81d6fb29857baf7b8908d4297773310
akopylovML <akopylov@megalabs.ru>
2023-05-02 16:47:33 +0300

    2nd lesson, Ex8 finished

--- l2/main.py
+++ l2/main.py
@@ -1,53 +1,18 @@
 import requests
+import time
 
-response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
-print(response.status_code)
-print(response.text)
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+token = response.json()["token"]
+timeout = response.json()["seconds"]
 
-response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "HEAD"})
-print(response.status_code)
-print(response.text)
-
-response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "GET"})
-print(response.status_code)
-print(response.text)
-
-methods = ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
-
-for method in methods:
-    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": f"{method}"})
-    # print(f"GET + {method} {response.status_code} {response.text}")
-    if method == "GET" and response.text != '{"success":"!"}':
-        print(f"Correct GET + {method} got failed response")
-    elif method != "GET" and response.text == '{"success":"!"}':
-        print(f"Wrong GET + {method} got correct response")
-    response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
-    # print(f"HEAD + {method} {response.status_code} {response.text}")
-    if response.text == '{"success":"!"}':
-        print(f"Wrong HEAD + {method} got correct response")
-    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
-    # print(f"POST + {method} {response.status_code} {response.text}")
-    if method == "POST" and response.text != '{"success":"!"}':
-        print(f"Correct POST + {method} got failed response")
-    elif method != "POST" and response.text == '{"success":"!"}':
-        print(f"Wrong POST + {method} got correct response")
-    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
-    # print(f"PUT + {method} {response.status_code} {response.text}")
-    if method == "PUT" and response.text != '{"success":"!"}':
-        print(f"Correct PUT + {method} got failed response")
-    elif method != "PUT" and response.text == '{"success":"!"}':
-        print(f"Wrong PUT + {method} got correct response")
-    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
-    # print(f"DELETE + {method} {response.status_code} {response.text}")
-    if method == "DELETE" and response.text != '{"success":"!"}':
-        print(f"Correct DELETE + {method} got failed response")
-    elif method != "DELETE" and response.text == '{"success":"!"}':
-        print(f"Wrong DELETE + {method} got correct response")
-    response = requests.options("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
-    # print(f"OPTIONS + {method} {response.status_code} {response.text}")
-    if response.text == '{"success":"!"}':
-        print(f"Wrong OPTIONS + {method} got correct response")
-    response = requests.patch("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
-    # print(f"PATCH + {method} {response.status_code} {response.text}")
-    if response.text == '{"success":"!"}':
-        print(f"Wrong PATCH + {method} got correct response")
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params = {"token": token})
+if response.json()["status"] != "Job is NOT ready":
+    print("Job has wrong status")
+time.sleep(timeout)
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params = {"token": token})
+if response.json()["status"] == "Job is ready" and "result" in response.json():
+    print("Job is OK")
+    print(response.json())
+else:
+    print("Job is broken")
+    print(response.json())
