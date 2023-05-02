fa9d26a4f710e2957085b685f030e77ecd7d8312
akopylovML <akopylov@megalabs.ru>
2023-05-02 16:19:49 +0300

    2nd lesson, Ex7 finished

--- l2/main.py
+++ l2/main.py
@@ -1,6 +1,53 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-print(response.history)
-print(len(response.history))
-print(response.history[1].url)
+response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
+print(response.status_code)
+print(response.text)
+
+response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "HEAD"})
+print(response.status_code)
+print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "GET"})
+print(response.status_code)
+print(response.text)
+
+methods = ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
+
+for method in methods:
+    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": f"{method}"})
+    # print(f"GET + {method} {response.status_code} {response.text}")
+    if method == "GET" and response.text != '{"success":"!"}':
+        print(f"Correct GET + {method} got failed response")
+    elif method != "GET" and response.text == '{"success":"!"}':
+        print(f"Wrong GET + {method} got correct response")
+    response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
+    # print(f"HEAD + {method} {response.status_code} {response.text}")
+    if response.text == '{"success":"!"}':
+        print(f"Wrong HEAD + {method} got correct response")
+    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
+    # print(f"POST + {method} {response.status_code} {response.text}")
+    if method == "POST" and response.text != '{"success":"!"}':
+        print(f"Correct POST + {method} got failed response")
+    elif method != "POST" and response.text == '{"success":"!"}':
+        print(f"Wrong POST + {method} got correct response")
+    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
+    # print(f"PUT + {method} {response.status_code} {response.text}")
+    if method == "PUT" and response.text != '{"success":"!"}':
+        print(f"Correct PUT + {method} got failed response")
+    elif method != "PUT" and response.text == '{"success":"!"}':
+        print(f"Wrong PUT + {method} got correct response")
+    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
+    # print(f"DELETE + {method} {response.status_code} {response.text}")
+    if method == "DELETE" and response.text != '{"success":"!"}':
+        print(f"Correct DELETE + {method} got failed response")
+    elif method != "DELETE" and response.text == '{"success":"!"}':
+        print(f"Wrong DELETE + {method} got correct response")
+    response = requests.options("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
+    # print(f"OPTIONS + {method} {response.status_code} {response.text}")
+    if response.text == '{"success":"!"}':
+        print(f"Wrong OPTIONS + {method} got correct response")
+    response = requests.patch("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": f"{method}"})
+    # print(f"PATCH + {method} {response.status_code} {response.text}")
+    if response.text == '{"success":"!"}':
+        print(f"Wrong PATCH + {method} got correct response")
